eac8ca42a2184a6b1f6f599c525d63f1322512a0
Donald Fox <don@foxnet1.local>
2017-07-14 17:14:08 -0500

    Friday Update4

--- logic.js
+++ logic.js
@@ -8,12 +8,52 @@ var config = {
 };
 firebase.initializeApp(config);
 var provider = new firebase.auth.GoogleAuthProvider();
-var x = 5;
-console.log(x);
+
+// Get Elements
+var txtEmail = document.getElementById('txtEmail');
+var txtPassword = document.getElementById('txtPassword');
+var btnLogin = document.getElementById('btnLogin');
+var btnRegister = document.getElementById('btnRegister');
+var btnLogout = document.getElementById('btnLogout');
+
+function LoginEmailPassword() {
+
+// Get email and password
+var email = txtEmail.value;
+var pass = txtPassword.value;
+var auth = firebase.auth();
+
+// Login with email and password
+  var promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(console.log(error.message));
+}
+
+//register event
+function registerWithEmailPassword() {
+
+// Get email and password
+var email = txtEmail.value;
+var pass = txtPassword.value;
+var auth = firebase.auth();
+// Login with email and password
+  var promise = auth.createUserWithEmailAndPassword(email, pass);
+  promise.catch(console.log(error.message));
+}
+
+// Real time listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if (firebaseUser) {
+    console.log(firebaseUser);
+    //btnLogout.classList.remove("hide");
+  }
+  else {
+    console.log("not logged in");
+    btnLogout.classList.add("hide");
+  }
+});
 
 function googleSignin() {
 
-   //console.log("test");
    firebase.auth().signInWithPopup(provider).then(function(result) {
       var token = result.credential.accessToken;
       var user = result.user;
