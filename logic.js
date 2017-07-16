48ce1428b25f400b5969f17d73cd154c46123178
Donald Fox <don@foxnet1.local>
2017-07-16 17:53:17 -0500

    Sunday Update

--- logic.js
+++ logic.js
@@ -18,12 +18,12 @@ var btnLogout = document.getElementById('btnLogout');
 
 function LoginEmailPassword() {
 
-// Get email and password
-var email = txtEmail.value;
-var pass = txtPassword.value;
-var auth = firebase.auth();
+  // Get email and password
+  var email = txtEmail.value;
+  var pass = txtPassword.value;
+  var auth = firebase.auth();
 
-// Login with email and password
+  // Login with email and password
   var promise = auth.signInWithEmailAndPassword(email, pass);
   promise.catch(console.log(error.message));
 }
@@ -31,50 +31,43 @@ var auth = firebase.auth();
 //register event
 function registerWithEmailPassword() {
 
-// Get email and password
-var email = txtEmail.value;
-var pass = txtPassword.value;
-var auth = firebase.auth();
-// Login with email and password
+  // Get email and password
+  var email = txtEmail.value;
+  var pass = txtPassword.value;
+  var auth = firebase.auth();
+  // Login with email and password
   var promise = auth.createUserWithEmailAndPassword(email, pass);
-  promise.catch(console.log(error.message));
+//  promise.catch(console.log(error.message));
 }
 
 // Real time listener
-firebase.auth().onAuthStateChanged(firebaseUser => {
-  if (firebaseUser) {
-    console.log(firebaseUser);
-    //btnLogout.classList.remove("hide");
-  }
-  else {
-    console.log("not logged in");
-    btnLogout.classList.add("hide");
-  }
+firebase.auth().onAuthStateChanged(firebaseUser).catch(function(error) {
+  console.log(error);
 });
 
 function googleSignin() {
 
-   firebase.auth().signInWithPopup(provider).then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
 
-      console.log(token);
-      console.log(user);
-   }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+    console.log(token);
+    console.log(user);
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
 
-      console.log(error.code);
-      console.log(error.message);
-   });
+    console.log(error.code);
+    console.log(error.message);
+  });
 }
 
 function googleSignout() {
-   firebase.auth().signOut()
+  firebase.auth().signOut()
 
-   .then(function() {
+    .then(function() {
       console.log('Signout Succesfull');
-   }, function(error) {
+    }, function(error) {
       console.log('Signout Failed');
-   });
+    });
 }
